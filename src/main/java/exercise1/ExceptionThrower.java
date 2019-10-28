2246151ef8007990f65d426fc628f15ad46f5cfd
182088 <Uriel.KANKULUFULUABU@student.umons.ac.be>
2019-10-28 10:41:59 +0100

    yeah 1

---
+++ src/main/java/exercise1/ExceptionThrower.java
@@ -0,0 +1,12 @@
+package exercise1;
+
+public class ExceptionThrower {
+
+	void throwACustomExceptionWhenValueIs42(int value) throws ExceptionValue42{
+		if(value==42){
+
+			throw new ExceptionValue42();
+		}
+
+	}
+}
---
+++ src/main/java/exercise1/ExceptionValue42.java
@@ -0,0 +1,9 @@
+package exercise1;
+
+public class ExceptionValue42 extends RuntimeException {
+
+	public void ExceptionValue42(){
+
+		System.out.println(" la valeur entrée est 42");
+	}
+}
